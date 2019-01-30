da05b7283c6c0eba79c7af77b7cda06f39e0e2a9
Majid Osman <omar12osman12@gmail.com>
2019-01-30 22:14:24 +0100

    Initial Commit

---
+++ machinelearningSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  machinelearningSwift
+//
+//  Created by Majid Osman on 2019-01-30.
+//  Copyright © 2019 Majid Osman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

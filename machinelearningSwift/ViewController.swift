0ef29a285a5341e51ea9699efc1148d2c248dd76
Majid Osman <omar12osman12@gmail.com>
2019-01-31 11:48:11 +0100

    smart camera

--- machinelearningSwift/ViewController.swift
+++ machinelearningSwift/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+ 
+     let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        guard  let CaptureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let Input = try? AVCaptureDeviceInput(device: CaptureDevice) else {return}
+        captureSession.addInput(Input)
+    captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "Videoqueue"))
+        
+//        let requests = VNCoreMLRequest(model: <#T##VNCoreMLModel#>, completionHandler: <#T##VNRequestCompletionHandler?##VNRequestCompletionHandler?##(VNRequest, Error?) -> Void#>)
+//        VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
+}
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("camera was able to to capture a frame", Data())
+    }
 
 }
 
